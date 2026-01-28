451d489ad58bdfab2e5a97b6cf24c6827f8175a9
Bigbilly-github <abiolatwitter97@gmail.com>
2026-01-28 15:58:40 +0100

    styling

--- app/index.tsx
+++ app/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/login/login">Go to Login</Link>
+      <Link href="/login/login" style={{ color: "red", fontSize: 18 , fontWeight: "bold"}}>Go to Login</Link>
     </View>
   );
 }
--- app/login/login.tsx
+++ app/login/login.tsx
@@ -1,11 +1,21 @@
-import { View , Text} from "react-native";
+import { View , Text, StyleSheet} from "react-native";
 
 export default function Login() {
   return (
     <>
-      <View>
-        <Text>Loginnnnnnnnnnnnnnnn</Text>
+      <View >
+        <Text style={styles.view}>Loginnnnnnnnnnnnnnnn</Text>
+        
       </View>
     </>
   );
 }
+
+
+const styles = StyleSheet.create({
+  view:{
+    color:"red",
+    fontSize:18,
+    fontWeight:"bold"
+  }
+})
